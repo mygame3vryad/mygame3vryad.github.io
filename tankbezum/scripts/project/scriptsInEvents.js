0369a0aca4e73a5c390dac2fd888fdeb61f58ef1
tankovgame <152401415+tankovgame@users.noreply.github.com>
2023-11-30 21:23:58 +0300

    Add files via upload

---
+++ tankbezum/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Game_Event2_Act30(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#27862d'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
